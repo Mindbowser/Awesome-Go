d5b8d674177274e61a7df9ad61ed4447d48e9575
ShaikhKabeer <kabeer.shaikh@mindbowser.com>
2017-07-31 17:44:13 +0530

    #INITIAL_COMMIT

---
+++ 01_gettingstarted/numericystem/numerics/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(42)
+	fmt.Printf("%d-%b \n", 42, 42)
+	fmt.Printf("%d - %b - %x \n", 42, 42, 42)
+	fmt.Printf("%d - %b - %#x \n", 42, 42, 42)
+	fmt.Printf("%d - %b - %#X \n", 42, 42, 42)
+	fmt.Printf("%d \t %b \t %#X \n", 42, 42, 42)
+
+	//Loop
+	for i := 1000000; i < 1000100; i++ {
+		fmt.Printf("%d \t %b \t %x \n", i, i, i)
+	}
+}
